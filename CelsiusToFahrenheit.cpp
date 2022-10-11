bb2600d968486eb00a7d433b04018bde514d8298
Srishty Mangutte <srishtymangutte@gmail.com>
2022-10-11 16:10:39 +0530

    Update CelsiusToFahrenheit.cpp

--- CelsiusToFahrenheit.cpp
+++ CelsiusToFahrenheit.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 { float C,F;
     cout<<"Enter temperature in Celsius:" " ";
     cin>>C;
-    F=(C*1.8)+32;
+    F=(C*9)/5 + 32;
     
-    cout<<C << " " <<"Degree Celsius in Fahrenheit is "<<F;
+    cout<<C << " " <<"Degree Celsius is "<<F<<" Farenheit.";
 
     return 0;
 }
